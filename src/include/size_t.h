f851e845f1c7305cf08946edd8c954cf0d790f6c
Antony Gordon <cuzintone@gmail.com>
2015-08-19 01:18:54 -0400

    Added include files

---
+++ src/include/size_t.h
@@ -0,0 +1,24 @@
+#ifndef _SIZE_T_H
+#define _SIZE_T_H
+
+//************************************************************************
+//
+// 			size_t.h
+//
+//			NIGHTDOS - 32-bit kernel
+//
+//************************************************************************
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Standard size_t type */
+
+typedef unsigned size_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/include/string.h
@@ -0,0 +1,20 @@
+#ifndef __STRING_H
+#define __STRING_H
+/*
+ * NightDOS Kernel
+ *
+ * string.h
+ *
+ *	Standard C String routines
+ *  Created on: Aug 19, 2015
+ *      Author: agordon
+ *
+ *
+ *
+ */
+
+#include <size_t.h>
+
+size_t strlen (const char* str );
+
+#endif /* __STRING_H */
