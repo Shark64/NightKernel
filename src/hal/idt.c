e25d03c10e9c7b103d9a683043959f1f3dc6edfa
Antony Gordon <cuzintone@gmail.com>
2015-08-20 19:53:36 -0400

    GDT/IDT code

---
+++ src/hal/idt.c
@@ -0,0 +1,9 @@
+/*
+ * idt.c
+ *
+ *  Created on: Aug 20, 2015
+ *      Author: agordon
+ */
+
+#include "idt.h"
+
---
+++ src/hal/idt.h
@@ -0,0 +1,13 @@
+/*
+ * idt.h
+ *
+ *  Created on: Aug 20, 2015
+ *      Author: agordon
+ */
+
+#ifndef __IDT_H
+#define __IDT_H
+
+
+
+#endif /* __IDT_H */
