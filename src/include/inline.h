0895bb9aeda2a0bd2fd408b4435bb549df5f593e
mercury0x000d <mercury0x000d@gmail.com>
2015-10-03 14:02:17 -0400

    Cleaned up more C remnants

--- src/include/inline.h
+++
@@ -1,24 +0,0 @@
-/**********************************************************
-
-		Inline assembly kludge fix for various C
-		Compilers
-
-***********************************************************/  
-  
-#ifdef _MSC_VER
-#define ASM _asm
-#else /*  */
-#ifdef __TURBO_C__
-#define ASM asm
-#else /*  */
-#ifdef __GNUC__
-#define ASM __asm__
-#endif /*  */
-#endif /*  */
-#endif /*  */
-  
-/*
-	End of inline defines
-	
-*/ 
-  
