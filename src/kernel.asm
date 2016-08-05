0c68cd8dff41ffd62b9dcfa1d53193821a8fac8b
maarten70 <netraam70@gmail.com>
2016-08-05 16:02:15 +0200

    Fixed two error's (see discription)
    - Fixed a mismatched operand sizes error at line 62
    - Fixed a wrong and undefined message, is replaced with reboot message.
    This is under label '.failed' at lines 93, 94, 95, 96, 97.
    
    Gongrats on reaching update 0.04!

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,20 +1,19 @@
-; Night DOS Kernel (kernel.asm) version 0.03
-; Copyright 1995-2015 by mercury0x000d
+; Night Kernel version 0.04
+; Copyright 1995 - 2016 by mercury0x000d
+; Kernel.asm is a part of the Night Kernel
 
-; Kernel.asm is a part of the Night DOS Kernel
-
-; The Night DOS Kernel is free software: you can redistribute it and/or
+; The Night Kernel is free software: you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License as published
 ; by the Free Software Foundation, either version 3 of the License, or (at
 ; your option) any later version.
 
-; The Night DOS Kernel is distributed in the hope that it will be useful, but
+; The Night Kernel is distributed in the hope that it will be useful, but
 ; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ; or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 ; for more details.
 
 ; You should have received a copy of the GNU General Public License along
-; with the Night DOS Kernel. If not, see <http://www.gnu.org/licenses/>.
+; with the Night Kernel. If not, see <http://www.gnu.org/licenses/>.
 
 ; See the included file <GPL License.txt> for the complete text of the
 ; GPL License by which this program is covered.
@@ -31,17 +30,18 @@
 
 bits 16
 
-org 0x0600                        ; set origin point to where the
-                                  ; FreeDOS bootloader loads this code
+; set origin point to where the FreeDOS bootloader loads this code
+org 0x0600
+
+; turn off interrupts and skip the GDT in a jump to our main routine
 cli
 jmp main
 
-%include "include/memdetect.asm"
-call starte820
 %include "gdt.asm"
 
 main:
-mov ax, 0x0000                    ; init the stack segment 
+; init the stack segment
+mov ax, 0x0000
 mov ss, ax
 mov sp, 0xffff
 
@@ -51,11 +51,81 @@ mov es, ax
 mov fs, ax
 mov gs, ax
 
+; get video controller info
+mov ax, 0x4F00
+mov di, VESAInfoBlock
+sti
+int 0x10
+cli
+cmp ax, 0x004F
+je GetModes
+jmp Hang
+
+GetModes:
+; step through the VESA modes available and find the best one available
+mov ds, [VESAInfoBlock.VideoModeListSegment]
+mov si, [VESAInfoBlock.VideoModeListOffset]
+mov edi, 0x00000000
+mov edx, 0x00000000
+.readLoop:
+mov cx, [si]
+
+;see if the mode = 0xffff so we know if we're all done
+cmp cx, 0xFFFF
+je .doneLoop
+
+; get info on that mode
+mov ax, 0x4F01
+mov di, VESAModeInfo
+sti
+int 0x10
+cli
+
+; skip this mode if it doesn't support a linear frame buffer
+cmp dword [VESAModeInfo.PhysBasePtr], 0x00000000
+je .doNextIteration
+
+push edx
+mov eax, 0x00000000
+mov ebx, 0x00000000
+mov word ax, [VESAModeInfo.YResolution]
+mov word bx, [VESAModeInfo.BytesPerScanline]
+mul ebx
+pop edx
+
+; loop again if this mode doesn't score higher than our current record holder
+cmp eax, edx
+jna .doNextIteration
+; it did score higher, so let's make note of it
+mov gs, cx
+mov edx, eax
+
+.doNextIteration:
+add si, 2
+jmp .readLoop
+.doneLoop:
+mov ax, 0xbeef
+
+; get info on the final mode
+mov ax, 0x4F01
+mov cx, gs
+mov di, VESAModeInfo
+sti
+int 0x10
+cli
+
+; set that mode
+mov ax, 0x4F02
+mov bx, cx
+sti
+int 0x10
+cli
+
+; load that GDT
 call load_GDT
 
-
-
-mov eax, cr0                      ; enter protected mode. YAY!
+; enter protected mode. YAY!
+mov eax, cr0
 or eax, 00000001b
 mov cr0, eax
 
@@ -72,15 +142,16 @@ idtStructure:
 
 
 kernel_start:
+
+; init the registers
 mov ax, 0x0010
 mov ds, ax
 mov es, ax
 mov ss, ax
 mov esp, 0x00090000
 
-
-
-mov eax, 0                        ; loop to init IDT
+; loop to init IDT
+mov eax, 0
 setupOneVector:
 push eax
 push 0x8e
@@ -98,49 +169,180 @@ lidt [idtStructure]
 
 
 
-%include "setints.asm"            ; set interrupt handler addresses
+; set interrupt handler addresses
+%include "setints.asm"
 
+; init and probe RAM
+;call MemoryInit
 
+; Fast A20 enable
+in al, 0x92
+or al, 0x02
+out 0x92, al
 
-push 0x07                         ; print splash message
-push 1
-push 1
-push kCopyright1
-call PrintString
-
-push 0x07
+; verify it worked
+in al, 0x92
+and al, 0x02
+cmp al, 0
+jnz kernelInitFastA20Success
+; it failed, so we have to say so
+push kFastA20Fail
+push 0xff777777
 push 2
+push 2
+call VESAPrintString
+jmp Hang
+kernelInitFastA20Success:
+
+; probe CPUID for vendor ID
+mov eax, 0x00000000
+cpuid
+mov esi, SystemInfo.CPUIDVendorString
+mov [SystemInfo.CPUIDLargestBasicQuery], eax
+mov [esi], ebx
+add esi, 4
+mov [esi], edx
+add esi, 4
+mov [esi], ecx
+
+; set VESA information into the SystemInfo structure
+mov byte dl, [VESAInfoBlock.VBEVersionMajor]
+mov byte [SystemInfo.VESAVersionMajor], dl
+
+mov byte dl, [VESAInfoBlock.VBEVersionMinor]
+mov byte [SystemInfo.VESAVersionMinor], dl
+
+mov eax, 0x00000000
+mov word ax, [VESAInfoBlock.OEMStringSegment]
+shl eax, 4
+mov ebx, 0x00000000
+mov bx, [VESAInfoBlock.OEMStringOffset]
+add eax, ebx
+mov dword [SystemInfo.VESAOEMStringPointer], eax
+
+mov dword edx, [VESAInfoBlock.Capabilities]
+mov dword [SystemInfo.VESACapabilities], edx
+
+mov word dx, [VESAModeInfo.XResolution]
+mov word [SystemInfo.VESAWidth], dx
+
+mov word dx, [VESAModeInfo.YResolution]
+mov word [SystemInfo.VESAHeight], dx
+
+mov byte dl, [VESAModeInfo.BitsPerPixel]
+mov byte [SystemInfo.VESAColorDepth], dl
+
+mov eax, 0x00000000
+mov word ax, [VESAInfoBlock.TotalMemory]
+mov ebx, 0x00010000
+mul ebx
+mov ebx, 0x00000400
+div ebx
+mov dword [SystemInfo.VESAVideoRAMKB], eax
+
+mov word dx, [VESAInfoBlock.OEMSoftwareRev]
+mov word [SystemInfo.VESAOEMSoftwareRevision], dx
+
+mov eax, 0x00000000
+mov word ax, [VESAInfoBlock.OEMVendorNameSegment]
+shl eax, 4
+mov ebx, 0x00000000
+mov bx, [VESAInfoBlock.OEMVendorNameOffset]
+add eax, ebx
+mov [SystemInfo.VESAOEMVendorNamePointer], eax
+
+mov eax, 0x00000000
+mov word ax, [VESAInfoBlock.OEMProductNameSegment]
+shl eax, 4
+mov ebx, 0x00000000
+mov bx, [VESAInfoBlock.OEMProductNameOffset]
+add eax, ebx
+mov [SystemInfo.VESAOEMProductNamePointer], eax
+
+mov eax, 0x00000000
+mov word ax, [VESAInfoBlock.OEMProductRevSegment]
+shl eax, 4
+mov ebx, 0x00000000
+mov bx, [VESAInfoBlock.OEMProductRevOffset]
+add eax, ebx
+mov [SystemInfo.VESAOEMProductRevisionPointer], eax
+
+mov eax, VESAInfoBlock.OEMData
+mov [SystemInfo.VESAOEMDataStringsPointer], eax
+
+mov dword edx, [VESAModeInfo.PhysBasePtr]
+mov dword [SystemInfo.VESALFBAddress], edx
+
+; probe CPUID for processor brand string
+mov eax, 0x80000000
+cpuid
+cmp eax, 0x80000004
+jnae CPUIDDoneProbing
+mov [SystemInfo.CPUIDLargestExtendedQuery], eax
+mov eax, 0x80000002
+cpuid
+mov esi, SystemInfo.CPUIDBrandString
+mov [esi], eax
+add esi, 4
+mov [esi], ebx
+add esi, 4
+mov [esi], ecx
+add esi, 4
+mov [esi], edx
+add esi, 4
+mov eax, 0x80000003
+cpuid
+mov [esi], eax
+add esi, 4
+mov [esi], ebx
+add esi, 4
+mov [esi], ecx
+add esi, 4
+mov [esi], edx
+add esi, 4
+mov eax, 0x80000004
+cpuid
+mov [esi], eax
+add esi, 4
+mov [esi], ebx
+add esi, 4
+mov [esi], ecx
+add esi, 4
+mov [esi], edx
+CPUIDDoneProbing:
+
+; print splash message - if we get here, we're all clear!
+push SystemInfo.kernelCopyright
+push 0xff777777
+push 2
+push 2
+call VESAPrintString
+
+; print number of int 15h entries
+;push memmap_ent
+push 0xB018BEEF
+push 0x07
+push 18
 push 1
-push kCopyright2
-call PrintString
+call VESAPrintHex
 
-push 0x07                         ; print number of int 15h entries
-push 3
-push 1
-push memmap_ent
-call PrintHex
-
-
-
-
-call PICInit                      ; setup and remap both PICs, enable ints
+; setup and remap both PICs, enable ints
+call PICInit
 call PICDisableIRQs
 call PICUnmaskAll
-
 call PITInit
-
-sti
+; sti
 
 infiniteLoop:
 jmp infiniteLoop
 
+Hang:
+jmp Hang
+
 
 
 %include "inthandl.asm"           ; interrupt handlers
-%include "idt.asm"                ; Interrupt Descriptor Table 
-
-%include "console.asm"            ; text console printing
+%include "idt.asm"                ; Interrupt Descriptor Table
 %include "hardware.asm"           ; hardware routines
-%include "pic.asm"                ; Programmable Interrupt Controller code
-
 %include "globals.asm"            ; global variable setup
+%include "memory.asm"             ; memory manager
---
+++ src/memory.asm
@@ -0,0 +1,132 @@
+; Night Kernel version 0.04
+; Copyright 1995 - 2016 by mercury0x000d
+; memory.asm is a part of the Night Kernel
+
+; The Night Kernel is free software: you can redistribute it and/or
+; modify it under the terms of the GNU General Public License as published
+; by the Free Software Foundation, either version 3 of the License, or (at
+; your option) any later version.
+
+; The Night Kernel is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+; or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+; for more details.
+
+; You should have received a copy of the GNU General Public License along
+; with the Night Kernel. If not, see <http://www.gnu.org/licenses/>.
+
+; See the included file <GPL License.txt> for the complete text of the
+; GPL License by which this program is covered.
+
+
+
+MemoryInit:
+ ; Sets up all data structures which will be used by the memory manager
+ ;  input:
+ ;   n/a
+ ;
+ ;  output:
+ ;   n/a
+
+ ; determine available memory
+
+ ; write memory manager globals
+ .starte820:
+ xor ebx, ebx 	;ebx needs to be set to 0
+ xor bp, bp      ;an entry count..........
+ mov edx, 0x0534D4150 ;smap needs to be set into edx
+ mov eax, 0xE820       ;eax needs to be E820
+ mov [es:di + 20], dword 1 ; so we are forcing a valid ACPI here
+ mov ecx, 24	;ask for 24 bytes
+ int 0x15		;call the int
+ jc short .failed
+ mov edx, 0x0534D4150
+ cmp eax, edx    ;eax gets reset to the 'smap' and edx is already that, so...
+ jne short .failed
+ test ebx, ebx  ;if ebx = 0 we failed, if ebx = 0 it's 1 entry long.
+ je short .failed
+ jmp short .next
+ 
+ .e820Continue:
+ mov eax, 0xe820 ;eax gets overwritten every time
+ mov [es:di + 20], dword 1
+ mov ecx, 24 ;ecx gets overwritten as well
+ int 0x15
+ jc short .e820f
+ mov edx, 0x0534D4150 ;repair the trashed register
+ 
+ .next:
+ jcxz .skipentry		;skip the entry which are 0 long
+ cmp cl, 20			;did we got a 24 byte ACPI response
+ jbe short .notext
+ test [es:di + 20], DWORD 1 ;if that's true is the ignore data bit clear
+ je short .skipentry
+ 
+ .notext:
+ mov ecx, [es:di + 8] ;get the lower memory region length
+ or ecx, [es:di + 12] ;or test for zero
+ jz .skipentry
+ 
+ .skipentry:
+ test ebx, ebx ;if it resets to 0 the list is done
+ jne short .e820Continue
+ 
+ .e820f:
+ mov [memmap_ent], bp ;store the entry count
+ clc					 ;clear the carry
+ ret
+ 
+ .failed:		;function unsupported
+ stc
+ push 0xff777777					; print failed to run kernel message
+ push 1
+ push 1
+ push kFailed
+ call VESAPrintString
+ 
+ push 0x07							; print function unsupported message
+ push 1
+ push 2
+ push kMeme820unsup
+ call VESAPrintString
+ 
+ push 0x07							; print reboot message
+ push 1
+ push 3
+ push kRebootMSG
+ call VESAPrintString
+ 
+ call Reboot
+ ret
+ memmap_ent db 0
+ret
+
+
+
+MemoryGet:
+ ; Returns the address of a block of memory of the specified size, or zero
+ ; if a block of that size is unavailble
+ ;  input:
+ ;   requested memory size in KB
+ ;
+ ;  output:
+ ;   address of requested block, or zero if call fails
+
+ret
+
+
+
+MemoryDispose:
+ ; Notifies the memory manager that the block specified by the address given
+ ; is now free for reuse
+ ;  input:
+ ;   starting address of block (obtained with MemoryGet)
+ ;
+ ;  output:
+ ;   n/a
+
+ret
+
+
+
+
